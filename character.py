ab6cc5f717c0cf710183313bb77b0dc2b8109fa7
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-10-17 01:17:33 +0900

    character클래스 멤버 변수 설정

--- character.py
+++ character.py
@@ -1,5 +1,12 @@
 from pico2d import *
+from frame import *
 
 class Character:
-    def __init__(self):
+    def __init__(self, alpha_image_path, frame_image_path):
+        self.xPos = 400
+        self.yPos = 90
+        self.frame = 0
+        self.face_dir = 1
+        self.image = load_image(alpha_image_path)
+        self.frame_list = get_frame_list(frame_image_path)
         pass
