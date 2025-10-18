2062a2ed1d910cb5dfc47325b3b9532f4e16a2d6
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-10-18 16:41:30 +0900

    Character클래스 구조 조정 -> 캐릭터마다 공격/이동 등의 프레임 넘버가 다르게 받기 위함

--- character.py
+++ character.py
@@ -2,11 +2,11 @@ from pico2d import *
 from frame import *
 
 class Character:
-    def __init__(self, alpha_image_path, frame_image_path):
+    def __init__(self, image_data):
         self.xPos = 400
         self.yPos = 90
         self.frame = 0
         self.face_dir = 1
-        self.image = load_image(alpha_image_path)
-        self.frame_list = get_frame_list(frame_image_path)
+        self.dir = 1
+        self.image=image_data
         pass
