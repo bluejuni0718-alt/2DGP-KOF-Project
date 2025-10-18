e1a89f843fc05c4a58e8dc8d75ee0a7ea2ad9148
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-10-18 17:45:40 +0900

    character_frame파일에서 함수 수정

--- character_frame.py
+++ character_frame.py
@@ -10,9 +10,9 @@ class KimFrameInfo:
         pass
     def draw_by_frame_num(self,frame_num, x,y,scale_x=None,scale_y=None):
         fx,fy,fw,fh=self.frame_list[frame_num]
-        self.print_image.draw_rect(fx, fy, fw, fh, x, y,scale_x, scale_y)
+        self.print_image.clip_draw(fx, fy, fw, fh, x, y,scale_x, scale_y)
         pass
     def draw_by_frame_num_composite(self,frame_num,x,y,scale_x=None,scale_y=None):
         fx,fy,fw,fh=self.frame_list[frame_num]
-        self.print_image.draw_clip_composite(fx,fy,fw,fh,0,'h',x,y,scale_x, scale_y)
+        self.print_image.clip_composite_draw(fx,fy,fw,fh,0,'h',x,y,scale_x, scale_y)
         pass
