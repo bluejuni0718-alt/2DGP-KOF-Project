2dbf57b6e02b9398278acbb7708bab5849a9ec46
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-10-18 17:15:52 +0900

    기본적인 main의 흐름 구조 생성

--- main.py
+++ main.py
@@ -7,5 +7,23 @@ isRun = True
 open_canvas()
 test_character = Character(KimFrameInfo())
 
+def handle_events():
+    pass
 
+def reset_world():
+    pass
+
+def update_world():
+    pass
+
+def render_world():
+    pass
+
+reset_world()
+
+while isRun:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.1)
 close_canvas()
