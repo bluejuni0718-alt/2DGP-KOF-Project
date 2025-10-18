295d081edb16ea21e8d4c00d536a3a326e7d9102
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-10-18 16:56:00 +0900

    KimFrameInfo클래스의 멤버 함수들 선언

--- character_frame.py
+++ character_frame.py
@@ -4,3 +4,7 @@ from frame import *
 class KimFrameInfo:
     def __init__(self):
         pass
+    def draw_by_frame_num(self,frame_num, x,y,scale_x=None,scale_y=None):
+        pass
+    def draw_by_frame_num_composite(self,frame_num,x,y,scale_x=None,scale_y=None):
+        pass
