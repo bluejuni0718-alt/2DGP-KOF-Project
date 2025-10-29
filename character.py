50277334b4cf453c733d46d21c6e1ba694e5f115
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-10-30 02:18:11 +0900

    jump시 방향키 입력 처리 변환 경우의 수 추가

--- character.py
+++ character.py
@@ -110,7 +110,7 @@ class Character:
             self.IDLE,{
                 self.IDLE:{self.right_down:self.WALK,self.left_down:self.WALK,self.up_down:self.JUMP},
                 self.WALK:{self.right_up:self.IDLE,self.left_up:self.IDLE},
-                self.JUMP:{time_out:self.IDLE}
+                self.JUMP:{time_out:self.IDLE,self.right_down:self.JUMP, self.left_down:self.JUMP, self.right_up:self.JUMP,self.left_up:self.JUMP},
             }
         )
     def update(self):
