cd4310024f5113880e51b5ae170be507c1489f69
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-10-31 00:14:05 +0900

    act종류에 따라 시작 프레임 지정하는 함수 선언 및 정의

--- character_frame.py
+++ character_frame.py
@@ -46,3 +46,9 @@ class KimFrameInfo:
         pass
     def draw_jump_move_by_frame_num(self,frame,x,y,face_dir):
         pass
+    def draw_by_act_kind(self,act_start_frame,act_frames,frame,x,y,face_dir):
+        fx, fy, fw, fh = self.frame_list[act_start_frame + frame]
+        if face_dir ==1:
+            self.print_image.clip_draw(fx, fy, fw, fh, x, y)
+        else:
+            self.print_image.clip_composite_draw(fx, fy, fw, fh, 0, 'h', x + self.delXPos, y + self.delYPos, fw, fh)
