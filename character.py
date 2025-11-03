29d05af11b346f502f1469f0bf6790aae46ddfa6
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-11-04 01:12:16 +0900

    상태 변환 수정

--- character.py
+++ character.py
@@ -231,7 +231,7 @@ class Character:
 
         self.state_machine = StateMachine(
             self.IDLE,{
-                self.IDLE:{self.right_double:self.RUN,self.left_double:self.RUN,self.right_down:self.WALK,self.left_down:self.WALK,self.up_down:self.JUMP},
+                self.IDLE:{self.right_double_fwd:self.RUN,self.left_double_fwd:self.RUN,self.right_down:self.WALK,self.left_down:self.WALK,self.up_down:self.JUMP},
                 self.WALK:{self.right_up:self.IDLE,self.left_up:self.IDLE,self.up_down:self.MOVE_JUMP},
                 self.JUMP:{time_out: self.IDLE, pressing_key:self.WALK},
                 self.MOVE_JUMP: {time_out:self.IDLE, pressing_key:self.WALK},
