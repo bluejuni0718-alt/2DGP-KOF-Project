7b4668eb98a9fa472b0128c49349d07dfa5df8ce
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-11-06 03:55:41 +0900

    객체 삭제 함수인 remove_object 정의

--- game_world.py
+++ game_world.py
@@ -7,6 +7,12 @@ def add_object(o,depth=0):
 def add_objects(ol, depth=0):
     world[depth] += ol
 
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+
 def update():
     for o in world:
         o.update()
