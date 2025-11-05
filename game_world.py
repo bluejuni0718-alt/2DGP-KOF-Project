c42d377157d676d4eea24dbf44c7fce894b8e355
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-11-06 03:54:51 +0900

    게임 월드에 객체들을 추가하는 하뭇 add_objects함수 선언

--- game_world.py
+++ game_world.py
@@ -4,6 +4,9 @@ def add_object(o,depth=0):
     global world
     world[depth].append(o)
 
+def add_objects(ol, depth=0):
+    world[depth] += ol
+
 def update():
     for o in world:
         o.update()
