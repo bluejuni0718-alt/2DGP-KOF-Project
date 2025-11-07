0b54987efcf1784b42a6d5843bc9c31fd19e7398
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-11-07 20:23:43 +0900

    main에서 알맞게 적용되도록 코드 수정

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pico2d import *
 import game_framework
-import intro_mode
+import intro_mode as character_select_mode
 
 open_canvas()
-game_framework.run(intro_mode)
+game_framework.run(character_select_mode)
 close_canvas()
