6b29a90789605d8562efe2bce1f316e8ba780483
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-11-07 21:38:43 +0900

    play_mode에서 확인할게 더많으니 기본적으로 play_mode로 시작하게끔 수정

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pico2d import *
 import game_framework
 import intro_mode as character_select_mode
+import play_mode
 
 open_canvas()
-game_framework.run(character_select_mode)
+#game_framework.run(character_select_mode)
+game_framework.run(play_mode)
 close_canvas()
