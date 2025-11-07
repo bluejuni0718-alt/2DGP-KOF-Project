3f9dd62a53d50f34d811d8c451772b06e1f0e3b9
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-11-07 20:19:47 +0900

    캐릭터 선택창 정상작동 확인 목적으로 handle_event함수는 간단히 작성

--- character_select_mode.py
+++ character_select_mode.py
@@ -14,6 +14,12 @@ def finish():
     pass
 
 def handle_events():
+    event_list=get_events()
+    for event in event_list:
+        if event.type== SDL_QUIT:
+            game_framework.quit()
+        elif event.type==SDL_KEYDOWN and event.key==SDLK_ESCAPE:
+            game_framework.quit()
     pass
 
 def draw():
