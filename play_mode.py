60c1a75048d91a205be9dac0e4018fadb2761563
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-11-17 19:21:16 +0900

    캐릭터 2명 생성 및 각각 키 설정

--- play_mode.py
+++ play_mode.py
@@ -1,7 +1,10 @@
 from pico2d import *
 from frame import *
 import game_framework
-from character import *
+from pico2d import *
+from frame import *
+import game_framework
+from character import Character, KimFrameInfo
 import game_world
 import intro_mode
 from interaction import InteractionManager
@@ -15,6 +18,28 @@ interaction_manager = InteractionManager(renderer=pico_renderer)
 interaction_manager.show_hitboxes = True
 
 running = True
+characters =[]
+KEYMAP_P1 ={
+    'left' :SDLK_a,
+    'right':SDLK_d,
+    'up'   :SDLK_w,
+    'down' :SDLK_s,
+    'lp'   :SDLK_f,
+    'rp'   :SDLK_g,
+    'rk'   :SDLK_b,
+    'lk'   :SDLK_c
+}
+KEYMAP_P2 ={
+    'left'  :SDLK_LEFT,
+    'right' :SDLK_RIGHT,
+    'up'    :SDLK_UP,
+    'down'  :SDLK_DOWN,
+    'lp'    :SDLK_KP_4,
+    'rp'    :SDLK_KP_5,
+    'rk'    :SDLK_KP_2,
+    'lk'    :SDLK_KP_1
+}
+
 
 def handle_events():
     event_list = get_events()
@@ -26,16 +51,21 @@ def handle_events():
             running = False
         elif event.type==SDL_KEYDOWN and event.key==SDLK_r:
             game_framework.change_mode(intro_mode)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_h:
+            # H 키로 히트박스 토글
+            interaction_manager.show_hitboxes = not getattr(interaction_manager, 'show_hitboxes', True)
         else:
-            test_character.handle_event(event)
+            for c in characters:
+                c.handle_event(event)
     pass
 
 def init():
-    global test_character
-
-    test_character = Character(KimFrameInfo())
-    game_world.add_object(test_character)
-    pass
+    global characters
+    c1 = Character(KimFrameInfo(), keymap=KEYMAP_P1, manager=interaction_manager, x=100, y=120)
+    c2 = Character(KimFrameInfo(), keymap=KEYMAP_P2, manager=interaction_manager, x=400, y=120)
+    characters = [c1, c2]
+    for c in characters:
+        game_world.add_object(c)
 
 def update():
     game_world.update()
