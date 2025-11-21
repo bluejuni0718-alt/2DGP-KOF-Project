da80dcde7c46a69b342b249cebb89be913d69382
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-11-22 02:28:07 +0900

    HitBoxManager 클래스 생성

--- interaction.py
+++ interaction.py
@@ -7,4 +7,12 @@ class HitBox:
         self.rect = rect
 
 class HitBoxManager:
-    pass
+    def __init__(self):
+        self.hitboxes = []
+
+    def register_hitbox(self, hitbox: HitBox):
+        self.hitboxes.append(hitbox)
+
+    def debug_draw(self):
+        for hb in self.hitboxes:
+            draw_rectangle(hb[0], hb[1], hb[0] + hb[2], hb[1] + hb[3])
