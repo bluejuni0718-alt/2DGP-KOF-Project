62ed778ea8d552bf56ca49390b046320f202d685
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-11-24 02:42:07 +0900

    히트박스의 충돌 여부 반환 함수 정의

--- interaction.py
+++ interaction.py
@@ -19,3 +19,9 @@ class HitBoxManager:
     def debug_draw(self):
         for hb in self.hitboxes:
             draw_rectangle(hb.rect[0], hb.rect[1], hb.rect[0] + hb.rect[2], hb.rect[1] + hb.rect[3])
+    def collision_check(self, hb1: HitBox, hb2: HitBox) -> bool:
+        r1 = hb1.rect
+        r2 = hb2.rect
+        x1, y1, w1, h1 = r1
+        x2, y2, w2, h2 = r2
+        return x1 < x2 + w2 and x1 + w1 > x2 and y1 < y2 + h2 and y1 + h1 > y2
