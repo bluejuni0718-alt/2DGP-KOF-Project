37b152e5484f1729050a63b27f7695237250c514
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-12-03 20:57:26 +0900

    기존 키맵 위치 변경

--- character_select_mode.py
+++ character_select_mode.py
@@ -1,32 +1,13 @@
 from pico2d import*
 import game_framework
 import play_mode
+import common
 
 
 image = None
 p1_image = None
 p2_image = None
 
-KEYMAP_P1 ={
-    'left' :SDLK_a,
-    'right':SDLK_d,
-    'up'   :SDLK_w,
-    'down' :SDLK_s,
-    'lp'   :SDLK_f,
-    'rp'   :SDLK_g,
-    'rk'   :SDLK_b,
-    'lk'   :SDLK_c
-}
-KEYMAP_P2 ={
-    'left'  :SDLK_LEFT,
-    'right' :SDLK_RIGHT,
-    'up'    :SDLK_UP,
-    'down'  :SDLK_DOWN,
-    'lp'    :SDLK_KP_4,
-    'rp'    :SDLK_KP_5,
-    'rk'    :SDLK_KP_2,
-    'lk'    :SDLK_KP_1
-}
 
 p1_x = 200
 p1_y = 50
@@ -60,25 +41,25 @@ def handle_events():
         if event.type== SDL_QUIT:
             game_framework.quit()
         elif event.type==SDL_KEYDOWN:
-            if event.key==KEYMAP_P1['left']:
+            if event.key==common.KEYMAP_P1['left']:
                 global p1_x
                 if min_x < p1_x - p_move_step < max_x and p1_selected == False:
                     p1_x = p1_x - p_move_step
-            elif event.key==KEYMAP_P1['right']:
+            elif event.key==common.KEYMAP_P1['right']:
                 if min_x < p1_x + p_move_step < max_x and p1_selected == False:
                     p1_x = p1_x + p_move_step
-            elif event.key==KEYMAP_P2['left']:
+            elif event.key==common.KEYMAP_P2['left']:
                 global p2_x
                 if min_x < p2_x - p_move_step < max_x and p2_selected == False:
                     p2_x = p2_x - p_move_step
-            elif event.key==KEYMAP_P2['right']:
+            elif event.key==common.KEYMAP_P2['right']:
                 if min_x < p2_x + p_move_step < max_x and p2_selected == False:
                     p2_x = p2_x + p_move_step
-            elif event.key == KEYMAP_P1['lk']:
+            elif event.key == common.KEYMAP_P1['lk']:
                 p1_rect[0] = 35
                 p1_selected = True
                 pass
-            elif event.key == KEYMAP_P2['lk']:
+            elif event.key == common.KEYMAP_P2['lk']:
                 p2_rect[0] = 100
                 p2_selected = True
                 pass
---
+++ common.py
@@ -0,0 +1,24 @@
+
+characters = []
+KEYMAP_P1 ={
+    'left' :SDLK_a,
+    'right':SDLK_d,
+    'up'   :SDLK_w,
+    'down' :SDLK_s,
+    'lp'   :SDLK_f,
+    'rp'   :SDLK_g,
+    'rk'   :SDLK_b,
+    'lk'   :SDLK_c
+}
+KEYMAP_P2 ={
+    'left'  :SDLK_LEFT,
+    'right' :SDLK_RIGHT,
+    'up'    :SDLK_UP,
+    'down'  :SDLK_DOWN,
+    'lp'    :SDLK_KP_4,
+    'rp'    :SDLK_KP_5,
+    'rk'    :SDLK_KP_2,
+    'lk'    :SDLK_KP_1
+}
+
+#todo: 다른 캐릭터 이미지 프레임 정보 여기다 넣기
