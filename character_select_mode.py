2b1c379ab972928e94108d2abdc88b057a9435c9
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-12-09 04:34:35 +0900

    xPos에 따라 선택한 캐릭터 번호 설정

--- character_select_mode.py
+++ character_select_mode.py
@@ -1,7 +1,9 @@
 from pico2d import*
 import game_framework
 import play_mode
+from character import Character
 import common
+from character_frame import *
 
 image = None
 p1_image = None
@@ -34,21 +36,19 @@ def finish():
     pass
 
 def handle_events():
-    global p1_rect, p2_rect, p1_selected, p2_selected
+    global p1_rect, p2_rect, p1_selected, p2_selected,p1_x,p2_x
     event_list=get_events()
     for event in event_list:
         if event.type== SDL_QUIT:
             game_framework.quit()
         elif event.type==SDL_KEYDOWN:
             if event.key==common.KEYMAP_P1['left']:
-                global p1_x
                 if min_x < p1_x - p_move_step < max_x and p1_selected == False:
                     p1_x = p1_x - p_move_step
             elif event.key==common.KEYMAP_P1['right']:
                 if min_x < p1_x + p_move_step < max_x and p1_selected == False:
                     p1_x = p1_x + p_move_step
             elif event.key==common.KEYMAP_P2['left'] and common.game_mode=='Two Player':
-                global p2_x
                 if min_x < p2_x - p_move_step < max_x and p2_selected == False:
                     p2_x = p2_x - p_move_step
             elif event.key==common.KEYMAP_P2['right']:
@@ -62,11 +62,32 @@ def handle_events():
                 p2_rect[0] = 100
                 p2_selected = True
                 pass
-        elif (p1_selected and p2_selected and common.game_mode=='Two Player')\
-                or (p1_selected and common.game_mode=='Single Player'):
+        elif p1_selected and p2_selected and common.game_mode=='Two Player':
+            if p1_x == 228:
+                common.p1_character_num = 0
+            elif p1_x == 403:
+                common.p1_character_num = 1
+            else:
+                common.p1_character_num = 2
+
+            if p2_x == 225:
+                common.p2_character_num = 0
+            elif p2_x == 400:
+                common.p2_character_num = 1
+            else:
+                common.p2_character_num = 2
+
+            game_framework.change_mode(play_mode)
+        elif p1_selected and common.game_mode=='Single Player':
+            if p1_x == 228:
+                common.p1_character_num = 0
+            elif p1_x == 403:
+                common.p1_character_num = 1
+            else:
+                common.p1_character_num = 2
             game_framework.change_mode(play_mode)
     pass
-#0,35,67,100
+#228,403, 578
 def draw():
     global image, p1_image, p2_image
     clear_canvas()
