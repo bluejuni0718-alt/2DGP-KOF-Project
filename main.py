92dcb60241a1a1bdc8128ee33379c3a991f9451c
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-12-09 04:35:27 +0900

    이제 intro부터 게임 시작

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import intro_mode as character_select_mode
 import play_mode
 
 open_canvas()
-#game_framework.run(character_select_mode)
-game_framework.run(play_mode)
+game_framework.run(character_select_mode)
+#game_framework.run(play_mode)
 close_canvas()
