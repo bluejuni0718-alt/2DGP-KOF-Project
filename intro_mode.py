b14a97b28e3894d906e3b56871257a7213ee5fca
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-12-10 21:25:35 +0900

    창에서 창닫을 수 있게 수정

--- intro_mode.py
+++ intro_mode.py
@@ -31,6 +31,8 @@ def handle_events():
     global running
     events = get_events()
     for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
         if event.type== SDL_KEYDOWN:
             if event.key== SDLK_1:
                 common.game_mode = 'Single Player'
